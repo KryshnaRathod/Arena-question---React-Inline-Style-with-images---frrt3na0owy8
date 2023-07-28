2145dcab261d91eb8ad9967a62340a25baca2510
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-28 15:00:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,13 @@ const App = () => {
   return (
     <div id="navbar">
       <div>NavBar</div>
-      <img />
+      <img   src={imageSrc} // Set the image source to the imported image
+          style={{
+            width: '130px', // Set the width of the image to '130px'
+            height: '100px', // Set the height of the image to '100px'
+            margin: '10px', // Set a margin of '10px' around the image
+          }}
+          alt="Navbar Image" />
     </div>
   )
 }
